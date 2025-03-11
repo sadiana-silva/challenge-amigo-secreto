c5b575b57efa06e4fbef2bece1d69b17b7e4b2c3
sadiana <dev.sadiana@gmail.com>
2025-03-10 21:06:20 -0300

    feat: function to update the friends list in the screen

--- app.js
+++ app.js
@@ -7,12 +7,37 @@ function adicionarAmigo() {
     // limpeza do campo de entrada
     document.getElementById('amigo').value = ''
 
-    //validação da entrada (interrompe a função caso o campo esteja em branco)
+    //validação: da entrada em branco
     if (nome === '') {
         alert('Por favor, insira um nome.')
+        return
+    }
+
+    // validação: nomes repetidos
+    if (amigos.includes(nome)) {
+        alert('Este nome já foi adicionado.')
         return
     }
     
     // adicionando nome à lista de amigos
     amigos.push(nome)
+
+    atualizarLista()
+}
+
+function atualizarLista() {
+    // captar a lista de exibição dos nomes
+    let lista = document.getElementById('listaAmigos')
+
+    // limpar a lista existente
+    lista.innerHTML = "" 
+
+    // loop para percorrer o array e exibir os nomes da lista
+    for (let i = 0; i < amigos.length; i++) {
+        let itemLista = document.createElement('li')
+        itemLista.textContent = amigos[i]
+
+        // adiciona um novo tópico à lista
+        lista.appendChild(itemLista)
+    }
 }
